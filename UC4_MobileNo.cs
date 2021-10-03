a6418b4b7cd0ba1214d6cf9487d7681cea076973
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-03 17:53:14 +0530

    [VaibhavChavan]Added:Validate Mobile number with country code using Regex

---
+++ UC4_MobileNo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_PP1
+{
+    class UC4_MobileNo
+    {
+        public static string Regex_mobilenum = "^[1-9][0-9][ ]?[6-9][0-9]{9}$";
+        public bool Validatemobile(string mobilenum)
+        {
+            return Regex.IsMatch(mobilenum, Regex_mobilenum);
+        }
+
+    }
+}
