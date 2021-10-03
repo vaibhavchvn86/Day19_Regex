3b8f2c340926e51eb16241f090dab0190370c36c
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-03 17:58:52 +0530

    [VaibhavChavan]Added:Validate Password with at least 1 upper case using Regex

---
+++ UC6_PassWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_PP1
+{
+    class UC6_PassWord
+    {
+        public static string Regex_password = "^[a-zA-Z]{8,}$";
+        public bool Validatepassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_password);
+        }
+
+    }
+}
