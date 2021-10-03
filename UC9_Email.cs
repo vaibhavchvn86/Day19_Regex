152358f33662aab004d55c3f21ba2ab7e189ba9a
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-03 18:06:09 +0530

    [VaibhavChavan]Added:Validate Sample Emails using Regex

---
+++ UC9_Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_PP1
+{
+    class UC9_Email
+    {
+        public static string Regex_Email = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, Regex_Email);
+        }
+
+    }
+}
