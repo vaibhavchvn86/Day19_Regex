d3839e973b25ef81e662ce180f3b6671f926041a
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-03 17:48:28 +0530

    [VaibhavChavan]Added:Validate Last name using Regex

---
+++ UC2_LastName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_PP1
+{
+    class UC2_LastName
+    {
+        public static string Regex_LName = "^[A-Z][a-z]{2,}$";
+        public bool ValidateLName(string Lastname)
+        {
+            return Regex.IsMatch(Lastname, Regex_LName);
+        }
+
+    }
+}
