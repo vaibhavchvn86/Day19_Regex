a1e66a47297c2cc4b3ab34041c822b0421afa5a2
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-10-11 17:54:47 +0530

    Update UC8_PassWD.cs

--- UC8_PassWD.cs
+++ UC8_PassWD.cs
@@ -7,7 +7,7 @@ namespace Day19_Regex_PP1
 {
     class UC8_PassWD
     {
-        public static string Regex_password = "^([a-zA-Z0-9]*[.@$#&_-][a-zA-Z0-9]*){8,}$";
+        public static string Regex_password = "^(?=.*[A-Z])(?=.*[!@#$%^&])([a-zA-Z0-9].{8,})$";
         public bool Validatepassword(string password)
         {
             return Regex.IsMatch(password, Regex_password);
